ef497a57d93131f67810387c232eb0525f57bf62
Victor Klimov <4klimov@gmail.com>
2021-01-29 19:19:46 +0300

    feat: solve What season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date === undefined) {
+      return 'Unable to determine the time of year!'
+    }
+    const isDate = date => date instanceof Date;
+    if (!isDate(date)) {
+      throw new Error('Not a Date');
+    }
+    const months = {
+        '1': 'winter',
+        '2': 'winter',
+        '3': 'spring',
+        '4': 'spring',
+        '5': 'spring',
+        '6': 'summer',
+        '7': 'summer',
+        '8': 'summer',
+        '9': 'autumn',
+        '10': 'autumn',
+        '11': 'autumn',
+        '12': 'winter'
+    }
+    const monthNumber = (date).toLocaleDateString().split('/')[0]
+    return months[monthNumber]
 };
