6af2b79d5b1ece913d860439a17c63a46a0bd3b9
Victor Klimov <4klimov@gmail.com>
2021-01-29 18:44:27 +0300

    feat: solve Carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    const activity = parseInt(sampleActivity, 10)
+    // Input validation
+    if (typeof sampleActivity !== 'string') return false
+    if (isNaN(activity)) return false
+    if (activity > MODERN_ACTIVITY || activity <= 0) return false
+    // Radioisotope Dating Formula     
+    return Math.log(activity / MODERN_ACTIVITY) * HALF_LIFE_PERIOD / 0.693
 };
