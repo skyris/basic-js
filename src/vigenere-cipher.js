8c1e0e0bb41ae60e8e5b3ea719a4dad8bcd27fa6
Victor Klimov <4klimov@gmail.com>
2021-02-02 18:19:47 +0300

    feat: solve Vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,71 @@
 const CustomError = require("../extensions/custom-error");
 
+
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.encryptTable = this._createTable();
+    this.decryptTable = this._createTable(false);
+  }
+
+  encrypt(message, key) {
+    const encrypt = true;
+    return this._encryptOrDecrypt(message, key, encrypt);
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(message, key) {
+    const encrypt = false;
+    return this._encryptOrDecrypt(message, key, encrypt);
+  }
+
+  _encryptOrDecrypt(message, key, encrypt) {
+    if (message === undefined || key === undefined) {
+      throw new Error('No arguments');
+    }
+    const getNextKeyChar = this._infiniteKeyGenerator(key);
+    const table = encrypt ? this.encryptTable : this.decryptTable;
+    message = message.toUpperCase();
+    const transformed = message.split('')
+      .map(el => (table[el] && table[el][getNextKeyChar()]) || el);
+    if (!this.direct) {
+      transformed.reverse();
+    }
+
+    return transformed.join('');
+  }
+
+  _createTable(encrypt = true) {
+    const ALPHABET_LENGTH = 26;
+    const CODE_OF_A = 65;
+    const alphabetArray = Array(ALPHABET_LENGTH)
+      .fill('')
+      .map((_, index) => index + CODE_OF_A);
+    const {fromCharCode: toChar} = String;
+    const table = {};
+    const setResultChar = encrypt
+      ? (row, column) => (row + column + ALPHABET_LENGTH) % ALPHABET_LENGTH + CODE_OF_A
+      : (row, column) => (row - column + ALPHABET_LENGTH) % ALPHABET_LENGTH + CODE_OF_A;
+
+    for (const num of alphabetArray) {
+      table[toChar(num)] = {};
+      for (const innerNum of alphabetArray) {
+        table[toChar(num)][toChar(innerNum)] = toChar(setResultChar(num, innerNum));
+      }
+    }
+    
+    return table;
+  }
+
+  _infiniteKeyGenerator(key) {
+    key = key.toUpperCase();
+    const {length} = key;
+    let count = 0;
+    return () => {
+      while (true) {
+        return key[count++ % length];
+      }
+    }
   }
 }
 
